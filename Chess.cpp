ecbf7546be9e912c70ab459a64ae47637bfcd24c
Darshan Don <darshandon.das465@gmail.com>
2018-03-19 01:12:48 +0530

    Add console view as front-end. - Read - Display - Move

--- Chess.cpp
+++ Chess.cpp
@@ -33,10 +33,16 @@ string getPieceName(Piece * piece) {
 	return name;
 }
 
-void printBoard() {
+void displayBoard() {
 	string name;
+
+	cout << "-------------------------------------------------------------------------------------------------------\n";
+	for (int j = 0; j < Board::MAX_COLUMNS; j++)
+		cout << "\t" << j;
+	cout << "\n\n";
 	for (int i = 0; i < Board::MAX_ROWS; i++)
 	{
+		cout << " " << i << "\t";
 		for (int j = 0; j < Board::MAX_COLUMNS; j++)
 		{
 			Piece * piece = chessBoard.getPieceAt(Position(i, j));
@@ -45,14 +51,148 @@ void printBoard() {
 		}
 		cout << "\n\n";
 	}
+	cout << "-------------------------------------------------------------------------------------------------------\n";
+	cout << "\n";
+}
+
+void displayInstructions()
+{
+	cout << "Welocme to Chess!\n\n";
+	cout << "The Capitalized pieces represents White and the other represents Black.\n";
+	cout << "Enter <Row Column> as (8,8) to quit.\n\n";
+}
+
+Position readSourcePosition()
+{
+	int sourceRow, sourceColumn;
+	cout << "Which Piece? <Row Column> : ";
+	cin >> sourceRow >> sourceColumn;
+	cout << "\n";
+
+	if (sourceRow == 8 && sourceColumn == 8)
+		exit(0);
+
+	Position sourcePosition = Position(sourceRow, sourceColumn);
+	if (!chessBoard.isInRange(sourcePosition))
+	{
+		cout << "Invalid Position\n\n";
+		readSourcePosition();
+	}
+	
+	return sourcePosition;
+}
+
+bool isValidSource(Position sourcePosition)
+{
+	Piece * pieceSelected = chessBoard.getPieceAt(sourcePosition);
+	if (pieceSelected == nullptr)
+	{
+		cout << "No Piece found at (" << sourcePosition.getRow() << "," << sourcePosition.getColumn() << ")\n\n";
+		return false;
+	}
+	else if (pieceSelected->getColor() != chessBoard.getCurrentPlayer())
+	{
+		cout << "The " << getPieceName(pieceSelected) << " at (" << sourcePosition.getRow() << "," << sourcePosition.getColumn() << ") belongs to the opponent.\n\n";
+		return false;
+	}
+	return true;
+}
+
+void displayValidMoves(vector<Position> validMoves)
+{
+	cout << "Valid Moves <Row Column> : ";
+	for (int i = 0; i < validMoves.size(); i++)
+		cout << "<" << validMoves[i].getRow() << " " << validMoves[i].getColumn() << ">    ";
+	cout << "\n\n";
+}
+
+Position readDestinationPosition(vector<Position> validMoves)
+{
+	int  destinatonRow, destinationColumn;
+	cout << "Where to? <Row Column> : ";
+	cin >> destinatonRow >> destinationColumn;
+	cout << "\n\n";
+
+	if (destinatonRow == 8 && destinationColumn == 8)
+		exit(0);
+
+	Position destinationPosition = Position(destinatonRow, destinationColumn);
+	for (int i = 0; i < validMoves.size(); i++)
+		if (destinatonRow == validMoves[i].getRow() && destinationColumn == validMoves[i].getColumn())
+			return destinationPosition;
+
+	cout << "Position (" << destinatonRow << "," << destinationColumn << ") is not a valid destination\n\n";
+	readDestinationPosition(validMoves);
+}
+
+void checkForWinner(Position destinationPosition)
+{
+	if (dynamic_cast<King*>(chessBoard.getPieceAt(destinationPosition)))
+	{
+		string playerColor;
+		if (chessBoard.getCurrentPlayer() == Piece::COLOR_WHITE)
+			playerColor = "WHITE";
+		else if (chessBoard.getCurrentPlayer() == Piece::COLOR_BLACK)
+			playerColor = "BLACK";
+		cout << "Player " << playerColor << " won!\n\n";
+
+		cout << "Type 'quit' to exit the game.\n";
+		string temp;
+		while (true)
+		{
+			cin >> temp;
+			if (temp == "quit")
+				exit(0);
+		}
+	}
+}
+
+void switchPlayer()
+{
+	switch (chessBoard.getCurrentPlayer())
+	{
+	case Piece::COLOR_WHITE:
+		chessBoard.setCurrentPlayer(Piece::COLOR_BLACK);
+		cout << "Player Black's turn\n\n";
+		break;
+	case Piece::COLOR_BLACK:
+		chessBoard.setCurrentPlayer(Piece::COLOR_WHITE);
+		cout << "Player White's turn\n\n";
+		break;
+	}
 }
 
 int main()
 {
-	cout << "Welocme to Chess!\n\n";
+	displayInstructions();
+	displayBoard();
 
-	printBoard();
-	cout << "\n\n";
+	cout << "Player White's turn\n\n";
+	Position sourcePosition, destinationPosition;
+	Piece * pieceSelected;
+	while (true)
+	{
+		sourcePosition = readSourcePosition();
+		if (isValidSource(sourcePosition))
+			pieceSelected = chessBoard.getPieceAt(sourcePosition);
+		else
+			continue;
+
+		vector<Position> validMoves = pieceSelected->getValidMoves(chessBoard, sourcePosition);
+		if (validMoves.size() == 0)
+		{
+			cout << "The Piece selected can't move.\n\n";
+			continue;
+		}
+		displayValidMoves(validMoves);
+
+		destinationPosition = readDestinationPosition(validMoves);
+		checkForWinner(destinationPosition);
+		chessBoard.movePieceTo(sourcePosition, destinationPosition);
+
+		displayBoard();
+		switchPlayer();
+	}
+
 	return 0;
 }
-
